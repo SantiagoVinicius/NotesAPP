f7e38478229fadb6cca995bf46d6f61785b93181
SantiagoVinicius <viniciussantiagosantos19@gmail.com>
2022-07-19 21:15:45 -0300

    teste

--- NotesApp/www/js/funcoes.js
+++ NotesApp/www/js/funcoes.js
@@ -1,12 +1,19 @@
 function novaAnotacao(){
     let texto = document.getElementById("anotacao").value
-    let linhaNova = `<div class="card-content white-text"><textarea>${texto}</textarea></div><div class="card-action center"><a id="botaoEditar">Editar</a><button onclick="excluirUmaLinha (this)">Deletar</button></div>`
+    let linhaNova = `<div class="card-content white-text"><textarea>${texto}</textarea></div><div class="card-action center"><button onclick="btEditar(this)">Editar</button><button onclick="excluirUmaLinha (this)">Deletar</button></div>`
     document.getElementById("anotacoesSalvas").innerHTML += linhaNova
 }
+
+
+function limpar(){
+    document.getElementById("anotacao").value = ""
+}
+
 
 let btSalvar = document.getElementById("botaoSalvar")
 btSalvar.addEventListener("click", function(){
     novaAnotacao()
+    limpar()
     salvar()
 })
 
@@ -14,10 +21,11 @@ function salvar(){
 localStorage.anotacoesSalvas = document.getElementById("anotacoesSalvas").innerHTML
 }
 
-function excluirUmaLinha(botao){
-    botao.parentNode.parentNode.remove()
-    salvar()
+//function excluirUmaLinha(botao)
+    //botao.parentNode.parentNode.remove()
+   // salvar()
 
+function btEditar(){
 }
 
 function carregar(){
@@ -26,7 +34,6 @@ function carregar(){
         localStorage.anotacoesSalvas
     }
 }
-
 
 window.addEventListener("load", function(){
     carregar()
